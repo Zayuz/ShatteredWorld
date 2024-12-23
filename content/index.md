31a28432cc38fa7d541bcd7d28a21c0b205f759c
Zayuz <Zayuz@hotmail.ca>
2024-12-22 22:29:45 -0500

    Quartz sync: Dec 22, 2024, 10:29 PM

--- content/index.md
+++ content/index.md
@@ -1,16 +1,4 @@
 ---
 title: Interactive Map
 ---
-```leaflet
-id:emjata-regional-map
-image: [[Emjata.png]]
-height: 768px
-unit: km
-lat: 50
-long: 50
-scale: 1
-minZoom: 8
-maxZoom: 12
-defaultZoom: 10.5
-zoomDelta: 0.5
-```
+[[map|Click to go to the interactive map!]]
--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "https://shatteredworld.org/",
+    baseUrl: "shatteredworld.org",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     generateSocialImages: false,
